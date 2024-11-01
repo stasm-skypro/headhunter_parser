5f22d0ff87ab87cee59742e8d1ede10430d3a166
Станислав Маяцкий <stasm226@gmail.com>
2024-11-01 21:12:32 +0500

    Implemented class to write csv-file

--- src/file_worker.py
+++ src/file_worker.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import os
 from abc import ABC, abstractmethod
@@ -6,7 +7,7 @@ from src.headhunter_api import HeadHunterAPI
 
 
 class FileWorker(ABC):
-    """Класс для работы с файлами: записи в файл данных и чтения даных из файла."""
+    """Класс для работы с файлами: записи в файл данных и чтения данных из файла."""
 
     @abstractmethod
     def write_file(self, data):
@@ -24,6 +25,28 @@ class JsonWriter(FileWorker):
             json.dump(data, file, ensure_ascii=False)
 
 
+class CsvWriter(FileWorker):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    @staticmethod
+    def get_field_names(data):
+        result = []
+        for key in data[0].keys():
+            result.append(key)
+        return result
+
+    def write_file(self, data):
+        field_names = self.get_field_names(data)
+        field_names.extend(['show_logo_in_search', 'branding'])
+
+        full_path = os.path.abspath(self.file_name)
+        with open(full_path, "w", newline='', encoding='UTF-8') as file:
+            writer = csv.DictWriter(file, fieldnames=field_names)
+            writer.writeheader()
+            for row_dict in data:
+                writer.writerow(row_dict)
+
 
 
 if __name__ == "__main__":
@@ -31,6 +54,11 @@ if __name__ == "__main__":
     hh_vacancies = hh_api.load_vacancies(keyword="Python", pages=1)
 
     # Запишем в json-файл
-    print("Запишем в json-файл")
-    fileworker = JsonWriter("../data/data.json")
+    # print("Запишем в json-файл", end='\n')
+    # fileworker = JsonWriter("../data/data.json")
+    # fileworker.write_file(hh_vacancies)
+
+    # Запишем в csv-файл
+    print("Запишем в csv-файл", end='\n')
+    fileworker = CsvWriter("../data/data.csv")
     fileworker.write_file(hh_vacancies)
