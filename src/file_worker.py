172516b230cbd025b727c65c3f45b92fb28192a8
Станислав Маяцкий <stasm226@gmail.com>
2024-11-05 22:20:30 +0500

    Reformated class FileWorker: added read_file method

--- src/file_worker.py
+++ src/file_worker.py
@@ -11,34 +11,39 @@ class FileWorker(ABC):
 
     @abstractmethod
     def write_file(self, data: list):
-        """Абстрактный метод для записи списка словарей в файл. Подлежит переопределению в классе-наследнике."""
+        """Абстрактный метод для json-объекта в файл. Подлежит переопределению в классе-наследнике."""
         ...
 
-    # @abstractmethod
-    # def get_data_from_file(self, filter_words: list[str]) -> list:
-    #     """Абстрактный метод для получения данных из файла по критериям"""
-    #
-    # @abstractmethod
-    # def delete_data_from_file(self,  id: str) -> None:
-    #     """Абстрактный метод для удаления данных из файла"""
+    @abstractmethod
+    def read_file(self):
+        """Абстрактный метод для записи json-объекта в файл. Подлежит переопределению в классе-наследнике."""
+        ...
 
 
-class JsonWriter(FileWorker):
-    """Класс для записи списка словарей в json-файл."""
+class JsonWorker(FileWorker):
+    """Класс для работы json-файлами."""
 
     def __init__(self, file_name: str) -> None:
         """Инициализатор экземпляра класса."""
         self.file_name = file_name
 
     def write_file(self, data: list) -> None:
-        """Записывает список словарей в json-файл."""
+        """Записывает json-объект в json-файл."""
         full_path = os.path.abspath(self.file_name)
         with open(full_path, "w", encoding="UTF-8") as file:
             json.dump(data, file, ensure_ascii=False)
 
+    def read_file(self):
+        """Читает содержимое json-файла в json-объект."""
+        full_path = os.path.abspath(self.file_name)
+        with open(full_path, "r", encoding='utf-8') as file:
+            data = json.load(file)
 
-class CsvWriter(FileWorker):
-    """Класс для записи списка словарей в csv-файл."""
+        return data
+
+
+class CsvWorker(FileWorker):
+    """Класс для работы с csv-файлами."""
 
     def __init__(self, file_name: str) -> None:
         """Инициализатор экземпляра класса."""
@@ -47,12 +52,10 @@ class CsvWriter(FileWorker):
     @staticmethod
     def get_field_names(data: list) -> list:
         """Создаёт список заголовков csv-файла."""
-        result = [key for key in data[0].keys()]
-
-        return result
+        return [key for key in data[0].keys()]
 
     def write_file(self, data: list) -> None:
-        """Записывает список словарей в csv-файл."""
+        """Записывает json-объект в csv-файл."""
         field_names = self.get_field_names(data)
         full_path = os.path.abspath(self.file_name)
         with open(full_path, "w", newline="", encoding="UTF-8") as file:
@@ -61,18 +64,38 @@ class CsvWriter(FileWorker):
             for row_dict in data:
                 writer.writerow(row_dict)
 
+    def read_file(self):
+        """Читает содержимое csv-файла в json-объект."""
+        data = []
+        full_path = os.path.abspath(self.file_name)
+        with open(full_path, "r", encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                data.append(dict(row))
 
-class ExcelWriter(FileWorker):
-    """Класс для записи списка словарей в xlsx-файл."""
+        return data
+
+
+class ExcelWorker(FileWorker):
+    """Класс для работы с xlsx-файлами."""
 
     def __init__(self, file_name: str) -> None:
+        """Инициализатор экземпляра класса."""
         self.file_name = file_name
 
     def write_file(self, data: list) -> None:
-        """Записывает список словарей в xlsx-файл."""
+        """Записывает json-объект в xlsx-файл."""
         df = pd.DataFrame(data)
         full_path = os.path.abspath(self.file_name)
         df.to_excel(full_path, index=False)
+
+    def read_file(self):
+        """Читает содержимое excel-файла в json-объект."""
+        full_path = os.path.abspath(self.file_name)
+        df = pd.read_excel(full_path)
+        data = df.to_dict(orient='records')
+
+        return data
 
 
 if __name__ == "__main__":
@@ -87,17 +110,32 @@ if __name__ == "__main__":
     # Из исходного списка словарей, полученных из API, соберём новый список словарей с выбранными ключами.
     validated_vacancies = [validator.validate(item) for item in hh_vacancies]
 
-    # Запишем в json-файл
-    print("Запишем в json-файл", end="\n")
-    json_worker = JsonWriter("../data/data.json")
+    # Запишем json-объект в json-файл
+    print("Запишем json-объект в json-файл", end="\n")
+    json_worker = JsonWorker("../data/data.json")
     json_worker.write_file(validated_vacancies)
 
-    # Запишем в csv-файл
-    print("Запишем в csv-файл", end="\n")
-    csv_worker = CsvWriter("../data/data.csv")
+    # Прочитаем json-файл в json-объект
+    print("Прочитаем json-файл в json-объект и выведем в консоль")
+    for item_data in json_worker.read_file():
+        print(item_data)
+
+    # Запишем json-объект в csv-файл
+    print("Запишем json-объект в csv-файл", end="\n")
+    csv_worker = CsvWorker("../data/data.csv")
     csv_worker.write_file(validated_vacancies)
 
-    # Запишем в Excel-файл
-    print("Запишем в Excel-файл", end="\n")
-    excel_worker = ExcelWriter("../data/data.xlsx")
+    # Прочитаем csv-файл в json-объект
+    print("Прочитаем csv-файл в json-объект и выведем в консоль")
+    for item_data in csv_worker.read_file():
+        print(item_data)
+
+    # Запишем json-объект в Excel-файл
+    print("Запишем json-объект в Excel-файл", end="\n")
+    excel_worker = ExcelWorker("../data/data.xlsx")
     excel_worker.write_file(validated_vacancies)
+
+    # Прочитаем Excel-файл в json-объект
+    print("Прочитаем Excel-файл в json-объект и выведем в консоль", end="\n")
+    for item_data in excel_worker.read_file():
+        print(item_data)
